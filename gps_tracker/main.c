41bfb320cb538667fb89a152227fee56a6419934
Greg Langford <greg@langford.me>
2018-01-21 14:30:58 +0000

    working with USART1

--- gps_tracker/main.c
+++ gps_tracker/main.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
+#include <string.h>
 #include "include/stm32f1xx.h"
+
+volatile uint8_t gps_start = 0;
+volatile uint8_t gps_ready = 0;
+volatile int bufpos = 0;
+char buf[100];
 
 void USART1_putchar(char c) {
     while(!(USART1->SR & USART_SR_TXE));
     USART1->DR = c;
+}
+
+void USART2_putchar(char c) {
+    while(!(USART2->SR & USART_SR_TXE));
+    USART2->DR = c;
 }
 
 void debug_message(char *data) {
@@ -12,26 +23,81 @@ void debug_message(char *data) {
     }
 }
 
+void debug_message_usart2(char *data) {
+    while(*data) {
+        USART2_putchar(*data++);
+    }
+}
+
 int main(void) {
+    RCC->CR |= RCC_CR_HSEON;                // HSE enable
+    while(!(RCC->CR & RCC_CR_HSERDY));      // Wait for HSE ready
+    RCC->CFGR &= ~(RCC_CFGR_SW);            // Clear configured clock
+    RCC->CFGR |= RCC_CFGR_SW_HSE;           // Configure clock to HSE
+
+    while((RCC->CFGR & RCC_CFGR_SWS) != RCC_CFGR_SWS_HSE);  // Wait for HSE clock
+
+    // Enable prepherials
+    RCC->APB1ENR |= RCC_APB1ENR_USART2EN;
     RCC->APB2ENR |= RCC_APB2ENR_AFIOEN | RCC_APB2ENR_IOPAEN | RCC_APB2ENR_IOPCEN | RCC_APB2ENR_USART1EN;
 
-    GPIOC->CRH &= ~(GPIO_CRH_CNF14) | ~(GPIO_CRH_CNF13);
-    GPIOC->CRH |= GPIO_CRH_MODE14_1 | GPIO_CRH_MODE13_1;
+    GPIOA->CRL &= ~(GPIO_CRL_CNF2);
+    GPIOA->CRL |= GPIO_CRL_CNF2_1;      // Alt func PP
+    GPIOA->CRL |= GPIO_CRL_MODE2;
 
-	GPIOC->ODR |= GPIO_ODR_ODR14 | GPIO_ODR_ODR13;
+    GPIOA->CRH &= ~(GPIO_CRH_CNF9);     // Clear CNF
+    GPIOA->CRH |= GPIO_CRH_CNF9_1;      // Alt func PP
+    GPIOA->CRH |= GPIO_CRH_MODE9;       // Output 50mhz
 
-    /* USART */
-    GPIOC->CRH &= ~(GPIO_CRH_CNF9);                     // Clear CNF9
-    GPIOA->CRH |= GPIO_CRH_CNF9_1;                      // Alternate Function Push-Pull
-    GPIOA->CRH |= GPIO_CRH_MODE9_0 | GPIO_CRH_MODE9_1;  // Output Max Speed 50mhz
+    USART1->BRR = 833;
+    USART1->CR1 = USART_CR1_UE | USART_CR1_RXNEIE | USART_CR1_TE | USART_CR1_RE;
 
-    USART1->BRR = 833;                                  // Baud 9600 (8000000/9600 = 833.33)
-    USART1->CR1 = USART_CR1_UE | USART_CR1_TE;          // UART Enable, UART TX Enable
+    USART2->BRR = 833;
+    USART2->CR1 = USART_CR1_UE | USART_CR1_RXNEIE | USART_CR1_TE | USART_CR1_RE;
 
-    char buffer[100];
-    sprintf(buffer, "This string has a number: %d\r\n", 100);
+    NVIC_EnableIRQ(USART1_IRQn);
+    NVIC_EnableIRQ(USART2_IRQn);
+
+    debug_message("Starting\r\n");
 
     while(1) {
-        debug_message(buffer);
+        if(gps_ready == 1) {
+            gps_ready = 0;
+
+            char *p;
+            p = strstr(buf, "$GNGGA");
+
+            if(p) {
+                debug_message_usart2(buf);
+                debug_message_usart2("\r\n");
+            }
+
+            // Reset GPS
+            memset(buf, 0, 100);
+            bufpos = 0;
+            gps_start = 0;
+            USART1->CR1 |= USART_CR1_RXNEIE;
+        }
+    }
+}
+
+void USART1_IRQHandler(void) {
+    char c;
+    if((USART1->SR & USART_SR_RXNE)) {
+        c = USART1->DR;
+
+        if(c == '$') {
+            gps_start = 1;
+        }
+
+        if(gps_start == 1) {
+            buf[bufpos] = c;
+            bufpos++;
+        }
+
+        if(c == '\r') {
+            USART1->CR1 &= ~(USART_CR1_RXNEIE);
+            gps_ready = 1;
+        }
     }
 }
